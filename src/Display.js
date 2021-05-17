2b95b8931b394255921570ca4f7b5b85e58a5356
Ted Teah <from@dabronx.nyc>
2021-05-17 16:23:41 -0400

    updates

--- src/Display.js
+++ src/Display.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Display = (props) => {
-  return <h1>Display</h1>;
+  const {dogs } = props
+  const loaded = () => (
+    <div style={{textAlign: "center"}}>
+      {dogs.map((dog) => (
+       <article key={dog._id}>
+       <img src={dog.img}/>
+       <h1>{dog.name}</h1>
+       <h3>{dog.age}</h3>
+       <button onClick={() => {
+            props.selectDog(dog)
+            props.history.push("/edit")
+          }}>
+            edit
+          </button>
+          <button onClick={() => {
+            props.deleteDog(dog)
+          }}>
+            Delete
+          </button>
+     </article>
+      ))}
+    </div>
+  )  
+  const loading = () => {
+   return  <h1> Loading . . . .</h1>
+    }
+    
+  return dogs.length > 0 ? loaded() : loading() 
 };
 
 export default Display;
